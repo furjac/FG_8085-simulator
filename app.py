f664684e92c81abd3c2db523e3745c2102e9151a
Furjack <81816200+furjac@users.noreply.github.com>
2025-04-12 20:07:33 +0530

    Changed some details to run smoothly on render

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, jsonify
-
+import os
 app = Flask(__name__)
 
 class Microprocessor8085:
@@ -1502,5 +1502,6 @@ def format_machine_code(opcode, memory, pc):
     return machine_code
 
 if __name__ == '__main__':
-    app.run(debug=True) 
+     port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port, debug=False)
 
