09452dae95320bc3115d2ca7084eea1bc17c7ffb
Furjack <81816200+furjac@users.noreply.github.com>
2025-04-12 20:10:09 +0530

    indentation fixed

--- app.py
+++ app.py
@@ -1502,6 +1502,6 @@ def format_machine_code(opcode, memory, pc):
     return machine_code
 
 if __name__ == '__main__':
-     port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port, debug=False)
 
